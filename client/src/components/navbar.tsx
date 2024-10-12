90547447abdc474d99200b440e28434d67f6f158
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-10-12 08:51:36 -0400

    Add home page link

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,11 +1,14 @@
-import { Trees } from "lucide-react";
+import {  Trees } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <header className="bg-green-600 text-white p-3 flex justify-between items-center shadow-md">
       <div className="flex items-center space-x-4">
         <Trees className="h-8 w-8" />
-        <h1 className="text-xl font-bold">EcoCompute</h1>
+        <Link href="/">
+          <h1 className="text-xl font-bold">EcoCompute</h1>
+        </Link>
       </div>
     </header>
   );
